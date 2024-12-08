3b7ee896dea9171aa20757b3c1b6ab7d15620cb5
raunak <raunakbiswas05@gmail.com>
2024-12-08 12:05:04 +0530

    Url requests from python

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+import requests
+import Important
+
+
+api_key = Important.get_api_key()
+url = Important.get_url()
+
+request = requests.get(url)
+content = request.text
+
+print(content)
