cd95f4fd1cb004ceb908fb6bb7b6b74fb285e15c
raunak <raunakbiswas05@gmail.com>
2024-12-08 12:20:18 +0530

    Reading and understanding the data from the API

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ api_key = Important.get_api_key()
 url = Important.get_url()
 
 request = requests.get(url)
-content = request.text
 
-print(content)
+#converting the data to dictionary
+content = request.json()
+
+#getting the details
+
+for article in content["articles"]:
+    print(f"title : {article["title"]}\ndescription: {article["description"]} \n-------------------------------------")
